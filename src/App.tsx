fe5f7c121cf15d9460147a37d4a6817839ce3ca7
yungan9 <geyun105@naver.com>
2025-03-19 10:29:13 +0900

    feat: define the route for the ProfileImgPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProfileImgPage from './pages/profile/ProfileImgPage';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route path="/profile1" element={<ProfileImgPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
