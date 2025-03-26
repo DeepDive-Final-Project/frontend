5b6f6655586a553ef9ba48591b77d6a8b1e253a2
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2025-03-26 21:04:18 +0900

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -7,20 +7,20 @@ import Kakao from '@/assets/images/kakao.svg';
 const handleKakao = () => {
   const url = "https://api.i-contacts.link/oauth2/authorization/kakao";
   window.location.href = url;
+  console.log("🥸 카카오 로그인 요청 URL:", url);
 };
-console.log("🥸 카카오 로그인 요청 URL:", url);
 
 const handleGoogle = () => {
   const url = "https://api.i-contacts.link/oauth2/authorization/google";
   window.location.href = url;
+  console.log("🥸 구글 로그인 요청 URL:", url);
 };
-console.log("🥸 구글 로그인 요청 URL:", url);
 
 const handleGit = () => {
   const url = "https://api.i-contacts.link/oauth2/authorization/github";
   window.location.href = url;
+  console.log("🥸 깃허브 로그인 요청 URL:", url);
 };
-console.log("🥸 깃허브 로그인 요청 URL:", url);
 
   return (
     <>
