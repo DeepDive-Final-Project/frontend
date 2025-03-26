88d37d00032792ec3ad521d4fc9a8e6fd85b6fd2
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2025-03-26 21:08:59 +0900

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,23 +4,24 @@ import Google from '@/assets/images/google.svg';
 import Github from '@/assets/images/github.svg';
 import Kakao from '@/assets/images/kakao.svg';
 
-const handleKakao = () => {
-  const url = "https://api.i-contacts.link/oauth2/authorization/kakao";
-  window.location.href = url;
-  console.log("🥸 카카오 로그인 요청 URL:", url);
-};
-
-const handleGoogle = () => {
-  const url = "https://api.i-contacts.link/oauth2/authorization/google";
-  window.location.href = url;
-  console.log("🥸 구글 로그인 요청 URL:", url);
-};
-
-const handleGit = () => {
-  const url = "https://api.i-contacts.link/oauth2/authorization/github";
-  window.location.href = url;
-  console.log("🥸 깃허브 로그인 요청 URL:", url);
-};
+const LoginPage = () => {
+  const handleKakao = () => {
+    const url = "https://api.i-contacts.link/oauth2/authorization/kakao";
+    window.location.href = url;
+    console.log("🥸 카카오 로그인 요청 URL:", url);
+  };
+  
+  const handleGoogle = () => {
+    const url = "https://api.i-contacts.link/oauth2/authorization/google";
+    window.location.href = url;
+    console.log("🥸 구글 로그인 요청 URL:", url);
+  };
+  
+  const handleGit = () => {
+    const url = "https://api.i-contacts.link/oauth2/authorization/github";
+    window.location.href = url;
+    console.log("🥸 깃허브 로그인 요청 URL:", url);
+  };
 
   return (
     <>
