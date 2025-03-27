e96db90e37a42e95691964f894de3db337cd349f
AnJuHyeon <105899818+AnJuHyeon@users.noreply.github.com>
2025-03-27 16:53:18 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths({
+  projects:['./tsconfig.app.json'],
+    })
+  ],
   define: {
     global: 'window',
   },
