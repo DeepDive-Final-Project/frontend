a7c4f90e96f89453c5f55abf6f9e3b43b9ad0292
yungan9 <geyun105@naver.com>
2025-03-31 00:30:26 +0900

    chore: add slide animation to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,15 @@ export default {
       fontFamily: {
         pretendard: ['pretendard'],
       },
+      keyframes: {
+        'slide-up': {
+          '0%': { transform: 'translateY(50px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'slide-up': 'slide-up 0.8s ease-out forwards',
+      },
     },
   },
   plugins: [
