21337acdd95cac7201d5eaabc219dbb89e751980
yungan9 <geyun105@naver.com>
2025-03-31 00:37:57 +0900

    feat: Implement Landing Title

---
+++ src/components/Landing/LandingTitle.tsx
@@ -0,0 +1,14 @@
+const LandingTitle = () => (
+  <>
+    <h1
+      className="text-center font-bold text-[28px] tablet:text-[60px] desktop:text-[72px]
+               leading-[42px] tablet:leading-[90px] desktop:leading-[108px] 
+               bg-gradient-to-l from-white via-[#ccc] to-[#999] 
+               bg-clip-text text-transparent font-pretendard">
+      내 주변에서 시작하는
+      <br />
+      쉽고 빠른 네트워킹
+    </h1>
+  </>
+);
+export default LandingTitle;
