f55d1032cfd1c7f66f1fdf03857b2732554bcbb5
yujsoo <jsyu333@gmail.com>
2025-03-31 15:54:41 +0900

    fix: resolve auth-login error

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_API_URL,
-  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     accept: 'application/json',
   },
 });
+
+axios.defaults.withCredentials = true;
