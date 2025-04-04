beecc562fca4d14cb61d2331b342cd61afccc01b
AnJuHyeon <anju3802@naver.com>
2025-04-04 12:17:59 +0900

    feat: reload

--- src/components/location/UserCard.tsx
+++ src/components/location/UserCard.tsx
@@ -4,29 +4,30 @@ import { useBottomSheetStore } from '@/stores/useBottomSheetStore';
 import { User } from '@/stores/useUserStore';
 import gradientOverlay from '@/assets/images/user_profile_radial_gradient.png';
 import Tag from './Tag';
-import Button from '@/components/common/Button';
 
 interface UserCardProps {
   user: User;
   onSelect: (userId: number) => void;
   selectedUserId: number | null;
-  buttonState: 'REQUEST' | 'WAITING' | 'MOVE' | 'CHATTED';
+  isRequested: boolean;
   onRequest: () => void;
-  onAccept?: () => void;
-  onReject?: () => void;
-  onMoveToChat?: () => void;
+  buttonLabel?: string;
+  onButtonClick?: () => void;
+  onRejectClick?: () => void;
 }
 
 const UserCard: React.FC<UserCardProps> = ({
   user,
   onSelect,
-  buttonState,
+  selectedUserId,
+  isRequested,
   onRequest,
-  onAccept,
-  onReject,
-  onMoveToChat,
+  buttonLabel,
+  onButtonClick,
+  onRejectClick,
 }) => {
   const [showTags, setShowTags] = useState(false);
+  const isSelected = selectedUserId === user.id;
   const mode = useBottomSheetStore((state) => state.mode);
 
   const handleCardClick = useCallback(
@@ -45,7 +46,15 @@ const UserCard: React.FC<UserCardProps> = ({
   return (
     <div className="relative w-full">
       <div
-        className={`user-card aspect-[3/5] rounded-xl flex flex-col text-white relative overflow-hidden border-2 border-gray-600 bg-[#0A0A0B] w-[clamp(130px,45vw,286px)] h-[clamp(226px,75vw,497.2px)] tablet:w-[clamp(130px,20vw,176.5px)] tablet:h-[clamp(226px,34vw,306.84px)] desktop:w-[176.5px] desktop:h-[306.84px]`}
+        className={`user-card aspect-[3/5]
+    rounded-xl flex flex-col text-white relative overflow-hidden border-2 border-gray-600 bg-[#0A0A0B]
+    w-[clamp(130px,45vw,286px)]
+    h-[clamp(226px,75vw,497.2px)]     
+
+    tablet:w-[clamp(130px,20vw,176.5px)]    
+    tablet:h-[clamp(226px,34vw,306.84px)]   
+
+    desktop:w-[176.5px] desktop:h-[306.84px] `}
         onClick={handleCardClick}>
         <div className="relative w-full h-48 rounded-lg overflow-hidden">
           <div
@@ -69,12 +78,12 @@ const UserCard: React.FC<UserCardProps> = ({
               </div>
               <button
                 onClick={handleToggleTags}
-                className="hover:opacity-80 rounded-full bg-[#18181b] px-2 py-2 tablet:px-3 tablet:py-3 transition-colors">
+                className="hover:opacity-80 rounded-full bg-[#18181b] px-1 py-1 mobile:px-2 mobile:py-2 tablet:px-3 tablet:py-3 transition-colors">
                 {showTags ? <ChevronUp /> : <ChevronDown />}
               </button>
             </div>
             {showTags && (
-              <div className="mt-2 p-2 mobile:p-3 tablet:p-4 text-xs mobile:text-sm tablet:text-base bg-neutral-900/30 backdrop-blur-[5px] rounded-lg shadow-[inset_0px_2px_12px_rgba(161,173,192,0.2)] outline outline-[0.5px] outline-offset-[-0.5px] outline-neutral-200/50">
+              <div className="mt-2 p-2  mobile:p-3 tablet:p-4 text-xs mobile:text-sm tablet:text-base bg-neutral-900/30 backdrop-blur-[5px] rounded-lg shadow-[inset_0px_2px_12px_rgba(161,173,192,0.2)] outline outline-[0.5px] outline-offset-[-0.5px] outline-neutral-200/50">
                 <div className="flex flex-col gap-1">
                   {user.tags.map((tag, index) => (
                     <Tag key={index} color={tag.color} text={tag.text} />
@@ -85,65 +94,99 @@ const UserCard: React.FC<UserCardProps> = ({
           </div>
         </div>
 
-        <div className="p-2 mt-2 flex flex-col gap-2 bg-[#0A0A0B] rounded-b-lg transition-all">
-          <button
-            onClick={(e) => e.stopPropagation()}
-            className="w-full px-2 py-2 rounded-full text-white bg-[#0A0A0B] hover:bg-[#146EF5] text-xs mobile:text-sm tablet:text-base">
-            상세 프로필 보기
-          </button>
+        {mode === 'explore' &&
+          (isSelected ? (
+            <div className="p-2 mt-2 flex flex-col gap-2 bg-[#0A0A0B] rounded-b-lg transition-all">
+              <button
+                onClick={(e) => e.stopPropagation()}
+                className="w-full px-2 py-2 rounded-full text-#E6E6E6 bg-[#0A0A0B] hover:bg-#146EF5 text-xs mobile:text-sm tablet:text-base">
+                상세 프로필 보기
+              </button>
 
-          {buttonState === 'CHATTED' && (
-            <Button size="sm" variant="secondary" disabled>
-              대화한 적 있음
-            </Button>
-          )}
-          {buttonState === 'WAITING' && (
-            <Button size="sm" variant="secondary" disabled>
-              수락 대기중...
-            </Button>
-          )}
-          {buttonState === 'MOVE' && (
-            <Button
-              size="sm"
-              onClick={(e) => {
-                e.stopPropagation();
-                onMoveToChat?.();
-              }}>
-              채팅방으로 이동
-            </Button>
-          )}
-          {buttonState === 'REQUEST' && mode === 'explore' && (
-            <Button
-              size="sm"
-              onClick={(e) => {
-                e.stopPropagation();
-                onRequest();
-              }}>
-              대화 요청하기
-            </Button>
-          )}
-          {mode === 'chat' && onAccept && onReject && (
-            <>
-              <Button
-                size="sm"
+              {buttonLabel === '채팅방으로 이동' && (
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    onButtonClick?.();
+                  }}
+                  className="w-full px-2 py-2 rounded-3xl text-#E6E6E6 bg-[#0A0A0B] hover:#146EF5 text-xs mobile:text-sm tablet:text-base">
+                  채팅방으로 이동
+                </button>
+              )}
+
+              {buttonLabel === '수락 대기중...' && (
+                <button
+                  disabled
+                  className="w-full px-2 py-2 rounded-3xl border border-#2C7DF6 text-#E6E6E6 bg-#0A0A0B cursor-not-allowed text-xs mobile:text-sm tablet:text-base">
+                  수락 대기중...
+                </button>
+              )}
+
+              {!buttonLabel && (
+                <button
+                  disabled={isRequested}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    onRequest();
+                  }}
+                  className={`w-full px-2 py-2 rounded-3xl text-#E6E6E6 transition-all
+            text-xs mobile:text-sm tablet:text-base
+            ${
+              isRequested
+                ? 'bg-gray-600 cursor-not-allowed'
+                : 'bg-[#0A0A0B] hover:bg-[#146EF5]'
+            }`}>
+                  {isRequested ? '요청 완료' : '대화 요청하기'}
+                </button>
+              )}
+            </div>
+          ) : (
+            <div className="p-2 mt-2 bg-[#0A0A0B] rounded-b-lg transition-all">
+              <p className="text-xs text-gray-300 mobile:text-sm tablet:text-base">
+                {user.introduction}
+              </p>
+            </div>
+          ))}
+
+        {mode === 'chat' && (
+          <div className="p-2 mt-2 flex flex-col gap-2 bg-[#0A0A0B] rounded-b-lg transition-all">
+            <button className="w-full px-2 py-2 rounded-full text-#E6E6E6 bg-[#0A0A0B] hover:bg-blue-600 text-xs mobile:text-sm tablet:text-base">
+              상세 프로필 보기
+            </button>
+
+            {buttonLabel === '수락하기' && (
+              <>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    if (onButtonClick) onButtonClick();
+                  }}
+                  className="w-full px-2 py-2 rounded-full text-#E6E6E6 bg-[#0A0A0B] text-xs mobile:text-sm tablet:text-base">
+                  수락하기
+                </button>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    if (onRejectClick) onRejectClick();
+                  }}
+                  className="w-full px-2 py-2 rounded-full text-#E6E6E6 bg-[#0A0A0B] text-xs mobile:text-sm tablet:text-base">
+                  거절하기
+                </button>
+              </>
+            )}
+
+            {buttonLabel === '채팅방으로 이동' && (
+              <button
                 onClick={(e) => {
                   e.stopPropagation();
-                  onAccept();
-                }}>
-                수락하기
-              </Button>
-              <Button
-                size="sm"
-                variant="secondary"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  onReject();
-                }}>
-                거절하기
-              </Button>
-            </>
-          )}
-        </div>
+                  if (onButtonClick) onButtonClick();
+                }}
+                className="w-full px-2 py-2 rounded-full text-#E6E6E6 bg-[#0A0A0B] hover:bg-blue-600 text-xs mobile:text-sm tablet:text-base">
+                채팅방으로 이동
+              </button>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
