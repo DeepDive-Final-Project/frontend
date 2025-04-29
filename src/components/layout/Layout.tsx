0aa7a8e90b1b1f7f7887771146f6ed62bec5c4c0
Jisoo Yu <jsyu333@gmail.com>
2025-04-29 23:49:58 +0900

    hotfix: add style code to header

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 const Layout = ({ nav }: { nav: React.ReactNode }) => {
   return (
     <>
-      <header className="fixed z-40 top-0 left-0 w-full h-[64px] flex items-center justify-between px-4 shrink-0 border-b border-[#1f1f1f] bg-[#000]">
+      <header className="pt-safe fixed z-40 top-0 left-0 w-full h-[64px] flex items-center justify-between px-4 shrink-0 border-b border-[#1f1f1f] bg-[#000]">
         {nav}
       </header>
       <div className="max-w-[1440px] m-auto pt-16">
