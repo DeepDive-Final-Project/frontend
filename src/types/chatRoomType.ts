8815e89bc1fb947967506c9e83e7a870dadf8d7c
Jisoo Yu <jsyu333@gmail.com>
2025-04-30 17:22:55 +0900

    fix: add last_sender_id (#173)

--- src/types/chatRoomType.ts
+++ src/types/chatRoomType.ts
@@ -3,6 +3,7 @@ export interface ChatRoomType {
   participants: string[];
   lastMessage: string;
   lastMessageTime: string | null;
+  lastSenderId:number;
   unreadCount: number;
   exited: boolean;
   otherId: number;
