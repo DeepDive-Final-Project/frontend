398f46f7ddf730cfdd913aea5126abedf0467214
Jisoo Yu <jsyu333@gmail.com>
2025-05-02 23:59:04 +0900

    fix: add custom connectHeaders to socket connection (#175)

--- src/stores/useSocketStore.ts
+++ src/stores/useSocketStore.ts
@@ -5,7 +5,12 @@ import SockJS from 'sockjs-client';
 interface StompState {
   stompClient: Client | null;
   isConnected: boolean;
-  connect: (onConnected?: () => void) => void;
+  connect: (
+    userId: number,
+    nickName: string,
+    roomId: number,
+    onConnected?: () => void,
+  ) => void;
   disconnect: () => void;
 }
 
@@ -13,12 +18,17 @@ export const useSocketStore = create<StompState>((set, get) => ({
   stompClient: null,
   isConnected: false,
 
-  connect: (onConnected) => {
+  connect: (userId, nickName, roomId, onConnected) => {
     console.log('웹소켓 연결 시도 중');
 
     const client = new Client({
       webSocketFactory: () => {
         return new SockJS(import.meta.env.VITE_WS_CHAT_URL);
+      },
+      connectHeaders: {
+        roomId: String(roomId),
+        clientId: String(userId),
+        senderNickname: nickName,
       },
       debug: (str) => console.log('[STOMP DEBUG]', str),
       reconnectDelay: 5000,
